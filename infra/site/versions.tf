d37e0edf6b6fa73032153eaf2c369fda868d3ebd
usr-bin-roygbiv <roy@davai.fyi>
2026-07-22 04:29:06 +0000

    chore(site): persist edge Terraform state

--- infra/site/versions.tf
+++ infra/site/versions.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 1.7.0, < 2.0.0"
+  backend "gcs" {
+    bucket = "roybench-tfstate-672373544179"
+    prefix = "t4code-com/prod"
+  }
+
 
   required_providers {
     google = {
